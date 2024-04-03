6012595441c047ef605eba5666e4d4b8fa6c919f
tonygc <antoniogonzalez@drata.com>
2024-04-03 12:49:18 -0600

    delete tf files [ENG-44162]

--- outputs.tf
+++
@@ -1,8 +0,0 @@
-# TODO: Remove this and add your own outputs
-output "nope" {
-  value = null_resource.nope.id
-}
-
-output "true" {
-  value = true
-}
--- versions.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  required_version = ">= 0.15"
-
-  # required_providers {
-  #   # TODO: define the providers required by this module
-  # }
-}
